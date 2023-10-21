824c62f5565057e4140b6a9afaccdecf79e1987b
alibaba0010 <zakariyahali100@gmail.com>
2023-10-21 13:46:31 +0100

    commiting

--- server/src/services/passport.js
+++ server/src/services/passport.js
@@ -1,9 +1,12 @@
 import passport from "passport";
 import GoogleStrategy from "passport-google-oauth20";
-
+import dotenv from "dotenv";
+dotenv.config();
+console.log(GOOGLE_CLIENT_ID);
 passport.use(
   new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   }),
   () => {}
 );
