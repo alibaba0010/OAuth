3a6f24a2de1c8ed3b9b1bccbb52b275558733f88
alibaba0010 <zakariyahali100@gmail.com>
2023-10-21 13:53:55 +0100

    setting up passport

--- server/src/services/passport.js
+++ server/src/services/passport.js
@@ -4,9 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 console.log(GOOGLE_CLIENT_ID);
 passport.use(
-  new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  }),
-  () => {}
+  new GoogleStrategy(
+    {
+      callbackURL: "/auth/google/redirect",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    () => {}
+  )
 );
