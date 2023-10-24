805b24f3cdebe914d358574296e2018a3e4d77a1
alibaba0010 <zakariyahali100@gmail.com>
2023-10-24 20:40:50 +0100

    serialize and deserialize passport, cookies

--- server/src/services/passport.js
+++ server/src/services/passport.js
@@ -3,6 +3,16 @@ import GoogleStrategy from "passport-google-oauth20";
 import dotenv from "dotenv";
 dotenv.config();
 import User from "../models/user.mongo.js";
+passport.serializeUser((user, done) => {
+  console.log("In serializeUser: ", user);
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  console.log("In deserializeUser: ", id);
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 export default passport.use(
   new GoogleStrategy(
     {
@@ -17,14 +27,22 @@ export default passport.use(
       console.log("Refresh Token: ", refreshToken);
       console.log("Profile: ", profile);
       const { id, displayName } = profile;
-      new User({
-        username: displayName,
-        googleId: id,
-      })
-        .save()
-        .then((newUser) => {
-          console.log("New User created: ", newUser);
-        });
+      User.findOne({ googleId: id }).then((currentUser) => {
+        console.log("In current User: ", currentUser);
+        if (currentUser) {
+          done(null, currentUser);
+        } else {
+          new User({
+            username: displayName,
+            googleId: id,
+          })
+            .save()
+            .then((newUser) => {
+              console.log("New User created: ", newUser);
+              done(null, newUser);
+            });
+        }
+      });
     }
   )
 );
