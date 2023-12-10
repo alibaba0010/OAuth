bbd37c60325c20794d194b192599569b14fc75be
alibaba0010 <zakariyahali100@gmail.com>
2023-12-10 07:58:38 +0100

    commit files

--- Github_no_Passport/ui/src/App.js
+++ Github_no_Passport/ui/src/App.js
@@ -1,40 +1,25 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import logo from "./logo.svg";
+import github from "./github";
 import "./App.css";
 
-const GITHUB_CLIENT_ID = "f04813941fc4ea88d8c7";
-const gitHubRedirectURL = "http://localhost:4000/api/v1/auth/github";
-const path = "/";
+// const GITHUB_CLIENT_ID = "f04813941fc4ea88d8c7";
+// const gitHubRedirectURL = "http://localhost:4000/api/v1/auth/github";
+// const path = "/";
 
-console.log("Hello");
 function App() {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    (async function () {
-      const usr = await axios
-        .get(`http://localhost:4000/api/v1/me`, {
-          withCredentials: true,
-        })
-        .then((res) => res.data);
-
-      setUser(usr);
-    })();
-  }, []);
+  // const [user, setUser] = useState();
 
   return (
     <div className="App">
-      {!user ? (
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${gitHubRedirectURL}?path=${path}&scope=user:email`}
-        >
-          LOGIN WITH GITHUB
-        </a>
-      ) : (
-        <h1>Welcome {user.login}</h1>
-      )}
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
     </div>
   );
 }
-
 export default App;
---
+++ Github_no_Passport/ui/src/github.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const github = () => {
+  return <div>github</div>;
+};
+
+export default github;
