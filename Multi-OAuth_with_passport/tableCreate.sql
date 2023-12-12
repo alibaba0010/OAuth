95a9eeddebb9566d4fcba5368fd13f019b61e976
alibaba0010 <zakariyahali100@gmail.com>
2023-12-12 10:45:16 +0100

    social login dir

---
+++ Multi-OAuth_with_passport/tableCreate.sql
@@ -0,0 +1,2 @@
+CREATE TABLE users (id serial PRIMARY KEY, user_id VARCHAR(100), provider VARCHAR(15), email VARCHAR(80) UNIQUE, password VARCHAR(100), full_name VARCHAR(50),
+    date_created timestamp with time zone DEFAULT CURRENT_TIMESTAMP, date_updated timestamp with time zone DEFAULT CURRENT_TIMESTAMP);
