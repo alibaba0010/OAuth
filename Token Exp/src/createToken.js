c63468d5ccba25e1cca06cf286fae1b2baca0922
alibaba0010 <zakariyahali100@gmail.com>
2023-12-19 20:37:08 +0100

    user func

--- Token Exp/src/createToken.js
+++ Token Exp/src/createToken.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { pool } from "./services/db.js";
-import { checkEmailExists } from "./services/Query.js";
+import { addUsers, checkEmailExists } from "./services/Query.js";
 import jwt from "jsonwebtoken";
-const createToken = Router();
 import dotenv from "dotenv";
+dotenv.config();
+const createToken = Router();
+
 createToken.post("/user", async (req, res) => {
   const { name, email } = req.body;
   try {
@@ -11,13 +13,17 @@ createToken.post("/user", async (req, res) => {
     if (checkIfEmailExists.rowCount !== 0)
       //  res.status(400).json({ errorMessage: "Email already exists" });
       throw new Error("Email already exists");
-
+    const addUser = await pool.query(addUsers, [name, email]);
     const signInToken = jwt.sign({ email: email }, process.env.JWT_SEC, {
       expiresIn: "1m",
     });
+    console.log(signInToken);
+    res
+      .status(201)
+      .json({ message: "User successfully crweated", signInToken });
   } catch (error) {
     res.json({ errorMessage: error });
   }
-  return signInToken;
+  //   return signInToken;
 });
 export default createToken;
--- Token Exp/src/services/Query.js
+++ Token Exp/src/services/Query.js
@@ -6,8 +6,9 @@ export const getUserByDBId = "SELECT * FROM users WHERE id = $1";
 export const checkEmailExists = "SELECT * FROM users  WHERE email = $1";
 // export const checkEmailExists = "SELECT s FROM users s WHERE s.email = $1";
 
-export const addUsers =
-  "INSERT INTO users (user_id, provider, email, password, full_name) VALUES ($1, $2, $3, $4, $5)";
+export const addUsers = "INSERT INTO users (name, email) VALUES ($1, $2)";
+// export const addUsers =
+//   "INSERT INTO users (user_id, provider, email, password, full_name) VALUES ($1, $2, $3, $4, $5)";
 
 export const deleteUserByID = "DELETE FROM users WHERE id = $1;";
 
